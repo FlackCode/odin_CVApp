4c343575e1b6eb3ac71d5e7bbaa7cd23f69f7cdd
flack <163582560+FlackCode@users.noreply.github.com>
2024-04-12 20:04:28 +0200

    Add files via upload

---
+++ src/components/Body.jsx
@@ -0,0 +1,11 @@
+import Template from "./Template";
+import TemplateEditor from "./TemplateEditor";
+
+export default function Body() {
+    return (
+        <>
+        <Template/>
+        <TemplateEditor/>
+        </>
+    )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <div className="py-4 bg-gray-100 shadow-lg">
+            <h1 className="font-bold text-2xl text-center">Resume Creator</h1>
+        </div>
+    )
+}
